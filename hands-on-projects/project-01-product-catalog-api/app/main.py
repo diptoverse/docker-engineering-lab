ba410fca2b0dcc9fbb3bcc6e22e37e19a9fc386f
Dipto Brata Das <diptod111@gmail.com>
2026-08-12 03:16:50 +0600

    ci - add Github Actions for Project 01

--- hands-on-projects/project-01-product-catalog-api/app/main.py
+++ hands-on-projects/project-01-product-catalog-api/app/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 
-from app.database import engine
-from app.models.base import Base
 from app.models.product import Product 
 from app.routers import products
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
+
 
 app.include_router(products.router)
 
